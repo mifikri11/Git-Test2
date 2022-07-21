a2ccb042f315941d2285080825c8f113df567c9f
ikhsanulfikri <ikhsanulfikri@intelix.co.id>
2022-07-21 11:53:35 +0700

    add list

--- index.php
+++ index.php
@@ -5,5 +5,9 @@
     <body>
         <h1>Auwo</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde error minus maxime aperiam nulla, ipsa quidem! Fuga itaque nostrum cupiditate cum dolore, fugit accusantium, qui omnis optio rem dolor repudiandae?</p>
+        <ul>
+            <li>Alberto Moreno</li>
+            <li>Left back</li>
+        </ul>
     </body>
 </html>
