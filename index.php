95719ff75617cb776604c1640054bd097430590d
ikhsanulfikri <ikhsanulfikri@intelix.co.id>
2022-07-21 11:46:23 +0700

    add paragraf di index.php

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
     </head>
     <body>
         <h1>Auwo</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde error minus maxime aperiam nulla, ipsa quidem! Fuga itaque nostrum cupiditate cum dolore, fugit accusantium, qui omnis optio rem dolor repudiandae?</p>
     </body>
 </html>
